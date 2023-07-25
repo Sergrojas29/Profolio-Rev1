2dedc5fd2882eef5b40afaa65fe620c6ff29cfa7
Sergio RA <Sergrojas29@gmail.com>
2023-07-25 18:28:49 -0400

    "Initial Layout"

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,5 +1,8 @@
 function test(){
+    console.log('test: ', test);
     const btnText = document.querySelector('.btnHeader')
     value = btnText.innerText
     console.log('value: ', value);
 }
+
+console.log(test());
